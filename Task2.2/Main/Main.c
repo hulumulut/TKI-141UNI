825d8c95f267731d9107db7c3ad8bc0aca6e4b2f
hulumulut <145440972+hulumulut@users.noreply.github.com>
2023-11-14 14:57:11 +0300

    Add files via upload

---
+++ Task2.2/Main/Main.c
@@ -0,0 +1,32 @@
+﻿#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int main() 
+{
+	// Значение a (заодно установка языка)
+	setlocale(LC_ALL, "Russian");
+	int a = 2.5;
+
+	// Ввод значение X
+	double x;
+	printf_s("введите значение X:");
+	scanf_s("%lf", &x);
+
+	// Решение разветленного уравнения
+	if (x > a || x <= a)
+	{
+		if (x > a) // первое решение, если значение x больше a
+		{
+			double y = x * pow(x - a, 1.0 / 3.0);
+			printf("Значение y равно с 1ым условием: %fl\n", y);
+		}
+		else if (x <= a) // второе решение, если значение x меньше или равно a
+		{
+			double y = x * sin(a * x);
+			printf("Значение y равно c 2ым условием: %fl\n", y);
+		}
+	}
+	return 0;
+}
