79d608da3004d09ca2149ca5c3bf46874aa2e4a9
hulumulut <145440972+hulumulut@users.noreply.github.com>
2023-12-12 14:37:17 +0300

    Add files via upload

--- Task3.1/Task3.1/Main.c
+++ Task3.1/Task3.1/Main.c
@@ -1,40 +1,78 @@
 ﻿#include <stdio.h>
 #include <math.h>
 #include <locale.h>
+#include <float.h>
 
-// Функция для вычисления значения функции
-double calculate_function(double x) 
+/**
+* @brief функция для поиска x
+* @return завершает функцию, выводя ответ
+*/
+double calculate_function(double x);
+
+/**
+* @brief функция, где присходит табуляция, а также вывод результата
+* @param заданные пользователем аргументы x_1, x_2, delta
+*/
+void tabulate_and_print();
+
+/**
+* @brief основная функция, где присходит вызов к таблице
+* @return завершение программы с выводом результата
+*/
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    // Вызов функции для табуляции и вывода результатов
+    tabulate_and_print();
+
+    return 0;
+}
+
+double calculate_function(double x)
 {
     return 3 * sin(sqrt(x)) + 0.39 * x - 3.8;
 }
 
-int main() 
+// Функция для табуляции функции и вывода результатов
+void tabulate_and_print()
 {
-    setlocale(LC_ALL, "Russian"); 
-    // Заданный интервал и шаг
-    double x_1 = 2.0;
-    double x_2 = 3.0;
-    double delta = 0.1;   
+    // Заданный интервал и шаг, вводимые пользователем
+    double x_1, x_2, delta;
+
+    printf("Введите начальное значение x (x_1): ");
+    if (scanf_s("%lf", &x_1) != 1) {
+        printf("Ошибка!\n");
+        return;
+    }
+
+    printf("Введите конечное значение x (x_2): ");
+    if (scanf_s("%lf", &x_2) != 1) {
+        printf("Ошибка!\n");
+        return;
+    }
+
+    printf("Введите шаг (delta): ");
+    if (scanf_s("%lf", &delta) != 1 || delta <= 0 || x_1 > x_2) {
+        printf("Ошибка!\n");
+        return;
+    }
 
     // Вывод заголовка таблицы
     printf("x\t|\ty\n");
     printf("--------------------\n");
 
-    // Табулирование функции 
-    for (double x = x_1; x <= x_2; x += delta) 
+    for (double x = x_1; x <= x_2; x += delta)
     {
         double result = calculate_function(x);
 
-        // Проверка на возможность расчета, является ли число числом, а не буквами или бесконечностью
-        if (!isnan(result) && !isinf(result)) 
+        // Проверка на возможность расчета с использованием DBL_EPSILON
+        if (!isnan(result) && !isinf(result))
         {
             printf("%.2lf\t|\t%.2lf\n", x, result);
         }
-        else 
+        else
         {
             printf("%.2lf\t|\tНет решения\n", x);
         }
     }
-
-    return 0;
 }
