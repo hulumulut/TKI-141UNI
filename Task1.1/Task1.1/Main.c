f8bd493df836f628f60b85711beb1c165742c5fe
hulumulut <145440972+hulumulut@users.noreply.github.com>
2023-12-19 14:14:12 +0300

    Add files via upload

--- Task1.1/Task1.1/Main.c
+++ Task1.1/Task1.1/Main.c
@@ -5,20 +5,18 @@
 #include <locale.h>
 /**
 * @brief ищем аргумент a
+* @param аргумент a, плюс заданное к нему уравнение
 */
-void get_a(float x, float y, float z)
-{
-	float a = (2 * cos(x - M_PI / 6)) / ((1 / 2) + pow(sin(y), 2)); // Формула a
-	printf_s("a=%f", a);
-}
+
+float get_a(float x, float y, float z);
+
 /**
 * @brief ищем аргумент b
+* @param аргумент b, плюс заданное к нему уравнение
 */
-void get_b(float x, float y, float z)
-{
-	float b = 1 + (pow(z, 2)) / (3 + (pow(z, 2) / 5)); // Формула b
-	printf_s("b=%f", b);
-}
+
+float get_b(float x, float y, float z);
+
 /**
 * @brief основная функция программа
 * @param get_a - параметры x,y,z для поиска аргумента a
@@ -26,7 +24,24 @@ void get_b(float x, float y, float z)
 */
 int main()
 {
+	const float x = 1.426;
+	const float y = -1.22;
+	const float z = 3.5;
 	setlocale(LC_ALL, "Russian");
-	get_a(1.426, -1.22, 3.5);
-	get_b(1.426, -1.22, 3.5);
+	printf_s("a=%f\n", get_a(x, y, z));
+	printf_s("b=%f\n", get_b(x, y, z));
+}
+
+
+
+float get_a(float x, float y, float z)
+{
+	float a = (2 * cos(x - M_PI / 6)) / ((1 / 2) + pow(sin(y), 2)); // Формула a
+	return a;
+}
+
+float get_b(float x, float y, float z)
+{
+	float b = 1 + (pow(z, 2)) / (3 + (pow(z, 2) / 5)); // Формула b
+	return b;
 }
