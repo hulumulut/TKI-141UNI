e72b5b065eb79392a540dea4c9ed3157de6b0fa7
hulumulut <145440972+hulumulut@users.noreply.github.com>
2023-12-19 14:36:42 +0300

    Add files via upload

--- Task1.2/Task1.2/Main.c
+++ Task1.2/Task1.2/Main.c
@@ -1,37 +1,47 @@
 #include <stdio.h>
 #include <locale.h>
 
-// Функция для ввода положительного числа
-float inputPositiveNumber(const char* prompt) 
-{
-    float number;
-    printf_s(prompt);
-    if (scanf_s("%f", &number) != 1 || number <= 0) 
-	{
-        printf_s("Некорректное значение. Пожалуйста, введите положительное число.\n");
-        return inputPositiveNumber(prompt);
-    }
-    return number;
-}
+/**
+* @brief Функция для ввода положительного числа, с проверкой этого числа
+* @param числа, заданные пользователем
+* @return завершает функцию с выводом числа пользователя, проверенного программой
+*/
 
-// Функция для вычисления длины второй стороны
-float B(float a, float n) 
-{
-    return n * a;
-}
+float inputPositiveNumber(const char* prompt);
 
-// Функция для вычисления периметра
-float Perimeter(float a, float b) 
-{
-    return 2 * (a + b);
-}
+/**
+* @brief Функция для вычисления длины второй стороны
+* @param числа, заданные пользователем и проверенные программой
+* @param число, которое находит программа путем заданного уравнения
+* @return завершает функцию с выводом результата
+*/ 
 
-// Функция для вычисления площади
-float Area(float a, float b) 
-{
-    return a * b;
-}
+float B(float a, float n);
 
+/**
+* @brief Функция для вычисления периметра
+* @param числа, заданные пользователем и проверенные программой
+* @param число, которое находит программа путем заданного уравнения
+* @return завершает функцию с выводом результата
+*/ 
+
+float Perimeter(float a, float b);
+
+/**
+* @brief Функция для вычисления площади
+* @param числа, заданные пользователем и проверенные программой
+* @param число, которое находит программа путем заданного уравнения
+* @return завершает функцию с выводом результата
+*/ 
+
+float Area(float a, float b);
+
+/**
+* @brief основная функция в программе
+* @param числа, заданные пользователем
+* @param числа, найденные программой
+* @return завершает программу с выводом данных
+*/
 int main() 
 {
     setlocale(LC_ALL, "Russian");
@@ -53,4 +63,33 @@ int main()
     printf("Площадь прямоугольника: %.2f\n", area);
 
     return 0;
+}
+
+
+
+float inputPositiveNumber(const char* prompt)
+{
+    float number;
+    printf_s(prompt);
+    if (scanf_s("%f", &number) != 1 || number <= 0)
+    {
+        printf_s("Некорректное значение. Пожалуйста, введите положительное число.\n");
+        return inputPositiveNumber(prompt);
+    }
+    return number;
+}
+
+float B(float a, float n)
+{
+    return n * a;
+}
+
+float Perimeter(float a, float b)
+{
+    return 2 * (a + b);
+}
+
+float Area(float a, float b)
+{
+    return a * b;
 }
