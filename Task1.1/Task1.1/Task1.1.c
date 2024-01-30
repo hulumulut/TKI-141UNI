44b08fe0820ae3ff797232f3838acb94165765a4
hulumulut <145440972+hulumulut@users.noreply.github.com>
2024-01-30 19:00:17 +0300

    Add files via upload
    Добавил описание return и сделал более структурнее

---
+++ Task1.1/Task1.1/Task1.1.c
@@ -0,0 +1,39 @@
+﻿#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <math.h>
+
+/**
+* @brief прототипы функий
+* @param аргументы, получаемые в ходе уравнений
+* @return получаем аргументы a и b
+*/
+float get_a(float x, float y, float z);
+float get_b(float x, float y, float z);
+
+/**
+* @brief основная функция 
+* @param аргументы, заданные условием
+*/
+int main()
+{
+    const float x = 1.426;
+    const float y = -1.22;
+    const float z = 3.5;
+
+    printf("a=%f\n", get_a(x, y, z));
+    printf("b=%f\n", get_b(x, y, z));
+
+    return 0;
+}
+
+float get_a(float x, float y, float z)
+{
+    float a = (2 * cos(x - M_PI / 6)) / ((1 / 2.0f) + pow(sin(y), 2));
+    return a;
+}
+
+float get_b(float x, float y, float z)
+{
+    float b = 1 + pow(z, 2) / (3 + pow(z, 2) / 5.0f);
+    return b;
+}
